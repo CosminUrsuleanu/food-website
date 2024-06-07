17eb1b9daa30cc54e54fc78d4d8e1f1cff34a9c5
CosminUrsuleanu <cosmin.ursuleanu92@gmail.com>
2024-06-07 06:58:49 -0900

    test

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./index.css";
+
+import Navbar from "./components/NavBar";
+import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
+import Food from "./components/Food";
+import Category from "./components/Category";
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <HeadlineCards />
+            <Food />
+            <Category />
+        </div>
+    );
+}
+
+export default App;
